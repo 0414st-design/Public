373477f1a15813f4a9a59cef0c1003874f910922
0414st-design <6222517st@gmail.com>
2026-05-31 15:26:53 +0900

    軽微な修正

--- environments/lab/locals.tf
+++ environments/lab/locals.tf
@@ -8,4 +8,8 @@ locals {
     Environment = local.environment
     ManagedBy   = "terraform"
   }
+
+  # ALB関連: モジュール外で管理するリソースの参照値
+  alb_acm_arn        = "arn:aws:acm:ap-northeast-1:123456789012:certificate/xxxx-xxxx"
+  alb_logs_bucket    = "mylab-alb-access-logs-lab"
 }
